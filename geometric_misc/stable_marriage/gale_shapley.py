6efded2c6e75495d0e8e976200b16e3c6b113e71
satemochi <satemochi1@yahoo.co.jp>
2020-03-27 10:41:22 +0900

    mod: gale_shapley arguments are limited

--- geometric_misc/stable_marriage/gale_shapley.py
+++ geometric_misc/stable_marriage/gale_shapley.py
@@ -1,54 +1,56 @@
 from collections import ChainMap, deque
 from pprint import pprint
-import random
+from random import seed, sample
 from matplotlib import pyplot as plt
 import networkx as nx
 
 
-def gen(n=10, seed=1):
-    g = nx.complete_bipartite_graph(n, n)
+def gen(n=10, s=1):
+    g, p = nx.complete_bipartite_graph(n, n), {}
     a, b = nx.bipartite.sets(g)
-    random.seed(seed)
+    seed(s)
     for u, v in zip(a, b):
-        g.nodes[u]['p'] = deque(random.sample(b, n))
-        g.nodes[v]['p'] = random.sample(a, n)
-    pprint(nx.get_node_attributes(g, 'p'))
-    return g
+        p[u] = deque(sample(b, n))
+        p[v] = sample(a, n)
+    pprint(p)
+    return g, p
 
 
-def get_pos(g):
-    return ChainMap(*[{v: [a*len(V), i] for i, v in enumerate(V)}
-                      for a, V in enumerate(nx.bipartite.sets(g))])
-
-
-def gale_shapley(g):
+def gale_shapley(p):
     m, (stack, _) = {}, nx.bipartite.sets(g)
     while stack:
         v = stack.pop()
-        w = g.nodes[v]['p'].popleft()
+        w = p[v].popleft()
         if w not in m:
             m[w] = v
         else:
-            u = m[w]
-            if g.nodes[w]['p'].index(v) < g.nodes[w]['p'].index(u):
-                stack.add(u)
+            if p[w].index(v) < p[w].index(m[w]):
+                if p[m[w]]:
+                    stack.add(m[w])
                 m[w] = v
-            else:
+            elif p[v]:
                 stack.add(v)
     return m.items()
 
 
-def draw(g, mat):
+def draw(g, matching):
     pos = get_pos(g)
     nx.draw_networkx_edges(g, pos, alpha=0.25)
     for v, c in zip(nx.bipartite.sets(g), ['#ffcccc', '#ccccff']):
         nx.draw_networkx_nodes(g, pos, nodelist=v, node_color=c)
-    nx.draw_networkx_edges(g, pos, edgelist=mat, edge_color='g', width=2)
+    nx.draw_networkx_edges(g, pos, edgelist=matching, edge_color='g', width=2)
     nx.draw_networkx_labels(g, pos)
+    plt.gca().axis('off')
+    plt.gca().set_aspect('equal')
     plt.savefig('gale_shapley_01.png', bbox_inches='tight')
     plt.show()
 
 
+def get_pos(g):
+    return ChainMap(*[{v: (a*len(V), i) for i, v in enumerate(V)}
+                      for a, V in enumerate(nx.bipartite.sets(g))])
+
+
 if __name__ == '__main__':
-    g = gen(10)
-    draw(g, gale_shapley(g))
+    g, p = gen(10)
+    draw(g, gale_shapley(p))
