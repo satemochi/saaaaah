bef942555d83a681e116601bb88c42727dddf65e
satemochi <satemochi1@yahoo.co.jp>
2026-03-21 19:38:49 +0900

    mod: minor for rational_search.py

--- geometric_misc/frechet_dist/rational_search/rational_search.py
+++ geometric_misc/frechet_dist/rational_search/rational_search.py
@@ -12,22 +12,22 @@ def rational_search(a, p, q, d):
             Fast Rational Search via Stern-Brocot Tree [arXiv] """
     assert p <= a <= q
     k = exponential_search(a, p, q, d)
-    higher, lower = binary_search(a, p, q, (1 << (k-1)) - 1, (1 << k) - 1, d)
+    x = binary_search(a, p, q, (1 << (k-1)) - 1, (1 << k) - 1, d)
     if d == direction.LEFT:
-        new_p = mpq(q.numerator + lower * p.numerator,
-                    q.denominator + lower * p.denominator)
-        new_q = mpq(q.numerator + (lower - 1) * p.numerator,
-                    q.denominator + (lower - 1) * p.denominator)
+        new_q = mpq(q.numerator + (x - 1) * p.numerator,
+                    q.denominator + (x - 1) * p.denominator)
         if new_q == a:
             return new_q
+        new_p = mpq(q.numerator + x * p.numerator,
+                    q.denominator + x * p.denominator)
         return rational_search(a, new_p, new_q, direction.RIGHT)
     else:
-        new_q = mpq(p.numerator + lower * q.numerator,
-                    p.denominator + lower * q.denominator)
-        new_p = mpq(p.numerator + (lower - 1) * q.numerator,
-                    p.denominator + (lower - 1) * q.denominator)
+        new_p = mpq(p.numerator + (x - 1) * q.numerator,
+                    p.denominator + (x - 1) * q.denominator)
         if new_p == a:
             return new_p
+        new_q = mpq(p.numerator + x * q.numerator,
+                    p.denominator + x * q.denominator)
         return rational_search(a, new_p, new_q, direction.LEFT)
 
 
@@ -67,7 +67,7 @@ def binary_search(a, p, q, higher, lower, d):
                 lower = split
             else:
                 higher = split
-    return higher, lower
+    return lower
 
 
 class test_rational_search(unittest.TestCase):
@@ -107,24 +107,22 @@ class test_rational_search(unittest.TestCase):
         a = mpq(2, 9)
         p, q = mpq(0), mpq(1)
         k = exponential_search(a, p, q, direction.LEFT)
-        _min, _max = binary_search(a, p, q, (1 << (k-1)) - 1, (1 << k) - 1,
-                                   direction.LEFT)
-        low = mpq(q.numerator + _max * p.numerator,
-                  q.denominator + _max * p.denominator)
-        high = mpq(q.numerator + _min * p.numerator,
-                   q.denominator + _min * p.denominator)
+        x = binary_search(a, p, q, (1 << (k-1))-1, (1 << k)-1, direction.LEFT)
+        low = mpq(q.numerator + x * p.numerator,
+                  q.denominator + x * p.denominator)
+        high = mpq(q.numerator + (x-1) * p.numerator,
+                   q.denominator + (x-1) * p.denominator)
         self.assertTrue(low <= a <= high)
 
     def test_binary_search_right(self):
         a = mpq(9, 14)
         p, q = mpq(0), mpq(1)
         k = exponential_search(a, p, q, direction.RIGHT)
-        higher, lower = binary_search(a, p, q, (1 << (k-1)) - 1, (1 << k) - 1,
-                                      direction.RIGHT)
-        high = mpq(p.numerator + higher * q.numerator,
-                   p.denominator + higher * q.denominator)
-        low = mpq(p.numerator + lower * q.numerator,
-                  p.denominator + lower * q.denominator)
+        x = binary_search(a, p, q, (1 << (k-1))-1, (1 << k)-1, direction.RIGHT)
+        high = mpq(p.numerator + (x-1) * q.numerator,
+                   p.denominator + (x-1) * q.denominator)
+        low = mpq(p.numerator + x * q.numerator,
+                  p.denominator + x * q.denominator)
         self.assertTrue(high <= a <= low)
 
     def test_rational_search_01(self):
