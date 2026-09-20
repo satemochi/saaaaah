f2ee3a150e1a0f627a7137ead064d7945e0a70c4
satemochi <satemochi1@yahoo.co.jp>
2026-09-20 17:57:52 +0900

    mod: minor for rlf_dsatur.py

--- geometric_misc/graph_coloring/rlf_dsatur.py
+++ geometric_misc/graph_coloring/rlf_dsatur.py
@@ -32,7 +32,7 @@ def dsatur(g):
         c = __get_color(sat[v])
         col[v], b = c, 1 << c
         for u in g[v]:
-            if u in col or sat[u] & b:
+            if u in col:
                 continue
             sat[u] |= b
             heappush(q, (-sat[u].bit_count(), d+1, u))
